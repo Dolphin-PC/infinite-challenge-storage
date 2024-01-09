018be85d4d9b1581bbac1d48a444fd8adba8e78d
dolphin-pc <pcx474@gmail.com>
2024-01-09 15:26:49 +0900

    episode findMany 오류

--- app/(back-end)/lib/services/episodeService.ts
+++ app/(back-end)/lib/services/episodeService.ts
@@ -1,6 +1,6 @@
-import { SeasonInterface } from "@/app/lib/types";
-import { DATA_LIMIT } from "@/app/lib/data";
-import { prismaClient } from "../db";
+import { SeasonInterface } from '@/app/lib/types'
+import { DATA_LIMIT } from '@/app/lib/data'
+import { prismaClient } from '../db'
 
 /**
  * 시즌 정보 목록 조회
@@ -8,18 +8,24 @@ import { prismaClient } from "../db";
  * @returns
  */
 export const getSeasonInfoList = async (
-  season?: string,
+  season?: string
 ): Promise<SeasonInterface[]> => {
-  const rows = await prismaClient.season_info.findMany({
-    where: {
-      season: {
-        equals: season,
-      },
-    },
-  });
+  let rows: SeasonInterface[] = []
 
-  return rows;
-};
+  if (season) {
+    rows = await prismaClient.season_info.findMany({
+      where: {
+        season: {
+          equals: season
+        }
+      }
+    })
+  } else {
+    rows = await prismaClient.season_info.findMany()
+  }
+
+  return rows
+}
 
 /**
  * 해당 시즌의 에피소드 목록 조회
@@ -30,28 +36,26 @@ export const getSeasonInfoList = async (
 export const getEpisodeInfoList = async (
   season: string | null,
   search: string | null,
-  pageNumber: number,
+  pageNumber: number
 ) => {
-
-
-  if (!season) season = "";
-  if (!search) search = "";
-  if (!pageNumber) pageNumber = 1;
+  if (!season) season = ''
+  if (!search) search = ''
+  if (!pageNumber) pageNumber = 1
 
   const condition = {
     where: {
       season: {
-        contains: season,
+        contains: season
       },
       AND: {
-        OR: [{ actor: { has: search } }, { title: { contains: search } }],
-      },
+        OR: [{ actor: { has: search } }, { title: { contains: search } }]
+      }
     },
     skip: (pageNumber - 1) * DATA_LIMIT,
-    take: DATA_LIMIT,
-  };
-  let rows = await prismaClient.episode_info.findMany(condition);
-  let rowCount = await prismaClient.episode_info.count(condition);
+    take: DATA_LIMIT
+  }
+  let rows = await prismaClient.episode_info.findMany(condition)
+  let rowCount = await prismaClient.episode_info.count(condition)
 
-  return { rows, rowCount };
-};
+  return { rows, rowCount }
+}
